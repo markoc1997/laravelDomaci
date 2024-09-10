a89fcff6a1229de1a5e37eff13babd8f7911fd19
markoc1997 <markocakmak55@gmail.com>
2024-09-10 12:42:25 +0200

    routing

---
+++ domaci1/resources/views/shop.blade.php
@@ -0,0 +1,1 @@
+<p>Shop Page</p>
--- domaci1/routes/web.php
+++ domaci1/routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
